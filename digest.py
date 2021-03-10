aa22d6856fe1217e819969327e403454a8f56a10
adelmofilho <adelmo.aguiar.filho@gmail.com>
2021-03-09 22:30:33 -0300

    minor fixes

--- digest.py
+++ digest.py
@@ -1,4 +1,5 @@
 import boto3
+import pipes
 client = boto3.client("ecr")
 details = client.describe_images(repositoryName='mlopsbot/tweets', imageIds=[{'imageTag': 'latest'}])
 digest = details["imageDetails"][0]["imageDigest"]
