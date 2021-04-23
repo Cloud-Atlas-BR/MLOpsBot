e7b1720869451755764b57ea6f8894eaf3971881
adelmofilho <adelmo.aguiar.filho@gmail.com>
2021-04-22 22:24:47 -0300

    fix: script build_push

--- scripts/build_push.sh
+++ scripts/build_push.sh
@@ -12,6 +12,6 @@ done
 
 aws ecr describe-repositories --repository-names ${repository} || aws ecr create-repository --repository-name ${repository}
 aws ecr get-login-password --region ${region} | docker login --username AWS --password-stdin ${account}.dkr.ecr.${region}.amazonaws.com
-docker build -t ${repository} context
+docker build -t ${repository} ${context}
 docker tag ${repository}:latest ${account}.dkr.ecr.${region}.amazonaws.com/${repository}:latest
 docker push ${account}.dkr.ecr.${region}.amazonaws.com/${repository}:latest
