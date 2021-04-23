0786185f5fc06cd6ef0d070c7fcb4af4eec5d06a
adelmofilho <adelmo.aguiar.filho@gmail.com>
2021-04-22 22:38:38 -0300

    test: skip feature

--- scripts/build_push.sh
+++ scripts/build_push.sh
@@ -1,17 +1,24 @@
 #!/bin/bash
 
-while getopts r:a:c:g: flag
+while getopts r:a:c:g:t: flag
 do
     case "${flag}" in
         r) repository=${OPTARG};;
         a) account=${OPTARG};;
         c) context=${OPTARG};;
         g) region=${OPTARG};;
+        t) test=${OPTARG};;
     esac
 done
 
-aws ecr describe-repositories --repository-names ${repository} || aws ecr create-repository --repository-name ${repository}
-aws ecr get-login-password --region ${region} | docker login --username AWS --password-stdin ${account}.dkr.ecr.${region}.amazonaws.com
 docker build -t ${repository} ${context}
-docker tag ${repository}:latest ${account}.dkr.ecr.${region}.amazonaws.com/${repository}:latest
-docker push ${account}.dkr.ecr.${region}.amazonaws.com/${repository}:latest
+
+if [ -z $test ]
+then
+    aws ecr describe-repositories --repository-names ${repository} || aws ecr create-repository --repository-name ${repository}
+    aws ecr get-login-password --region ${region} | docker login --username AWS --password-stdin ${account}.dkr.ecr.${region}.amazonaws.com
+    docker tag ${repository}:latest ${account}.dkr.ecr.${region}.amazonaws.com/${repository}:latest
+    docker push ${account}.dkr.ecr.${region}.amazonaws.com/${repository}:latest
+else
+    echo "Push Skipped"
+fi
