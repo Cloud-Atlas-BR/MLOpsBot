c29ef2fcf419895df35d66ca2d5b3dd475294330
adelmofilho <adelmo.aguiar.filho@gmail.com>
2021-04-25 19:37:22 -0300

    fix pytest installation

--- scripts/build_push.sh
+++ scripts/build_push.sh
@@ -17,6 +17,7 @@ if [ $test == True ]
 then
     docker build --build-arg ACCESS_KEY=${key} --build-arg SECRET_ACCESS=${secret} --build-arg REGION=${region} -t ${repository} ${context}
     docker run -d -p 9000:8080 ${repository}
+    pip install pytest
     pytest
 fi
 
