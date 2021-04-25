92d8d64962b6fda1d71e72dd7b6d1f48f695bd02
adelmofilho <adelmo.aguiar.filho@gmail.com>
2021-04-24 21:42:45 -0300

    hotix: tem que ser isso

--- scripts/image_digest.sh
+++ scripts/image_digest.sh
@@ -10,4 +10,4 @@ done
 sudo apt-get install -y jq -qq > /dev/null
 aws ecr describe-images --repository-name ${repository} --image-ids imageTag=latest > output.json
 digest=$(jq '.imageDetails[0].imageDigest' output.json)
-echo $digest
+echo "$digest" | sed -e 's/^"//' -e 's/"$//'
