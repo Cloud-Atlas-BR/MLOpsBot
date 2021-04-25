4684f507b7eb1a039fb47673e1cda8cc34487fb8
adelmofilho <adelmo.aguiar.filho@gmail.com>
2021-04-24 21:38:54 -0300

    hotfix: vai e eu acredito

--- scripts/image_digest.sh
+++ scripts/image_digest.sh
@@ -7,7 +7,7 @@ do
     esac
 done
 
-sudo apt-get install -y jq
+sudo apt-get install -y jq -qq > /dev/null
 aws ecr describe-images --repository-name ${repository} --image-ids imageTag=latest > output.json
 digest=$(jq '.imageDetails[0].imageDigest' output.json)
 echo $digest
