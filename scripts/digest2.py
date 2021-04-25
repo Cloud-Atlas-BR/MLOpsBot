0743b05924b148a00b31744d772d0cb71487b50c
adelmofilho <adelmo.aguiar.filho@gmail.com>
2021-04-24 21:12:08 -0300

    update dpeloy code

--- scripts/digest2.py
+++
@@ -1,6 +0,0 @@
-import boto3
-import pipes
-client = boto3.client("ecr")
-details = client.describe_images(repositoryName='mlopsbot/retweets', imageIds=[{'imageTag': 'latest'}])
-digest = details["imageDetails"][0]["imageDigest"]
-print("export DIGEST_RETWEET=%s" % (pipes.quote(str(digest))))
